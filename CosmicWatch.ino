6a9232394ccfc3f88d2c617c2a36a2a9b3ef1f0d
Canis Li <73449574+canisli@users.noreply.github.com>
2023-10-12 15:40:32 -0700

    Added interrupt

--- CosmicWatch.ino
+++ CosmicWatch.ino
@@ -1,12 +1,12 @@
 // definitions
-io1 = ... // digital input pin for the AND gate
+io0 = ... // digital input pin for the AND gate
 a1 = ... // analog input pin for the first SiPM
 a2 = ... // analog input pin for the second SiPM
 a3 = ... // analog input pin for the third SiPM
-do1 = … digital output of the reset module
-do2 = ... led1
-do3 = ... led2
-do4 = ... lde3 
+do0 = … digital output of the reset module
+do1 = ... led1
+do2 = ... led2
+do3 = ... lde3 
 threshold = ... // ampltiude threshold to flash LED
 myFile = ... // location to SD card file
 
@@ -14,9 +14,6 @@ myFile = ... // location to SD card file
 int get_sipm_voltage(adc):
   // ...
 }
-
-
-
 
 // sipm1 -> | -> a1
 //        |
@@ -29,19 +26,39 @@ int get_sipm_voltage(adc):
 
 // make somes tests for flashing LED and writing to file
 
+// *************************************************************************************************
 void setup(){
-  pinMode(do2, OUTPUT);//initialize digital pin LED_BUILTIN as an output
+  pinMode(io0, INPUT)
+  pinMode(a1, INPUT)
+  pinMode(a2, INPUT)
+  pinMode(a3, INPUT)
+  pinMode(do0, OUTPUT);
+  pinMode(do1, OUTPUT);//initialize digital pin LED_BUILTIN as an output
+  pinMode(do2, OUTPUT);
   pinMode(do3, OUTPUT);
-  pinMode(do4, OUTPUT);
-}
 
-void run_when_interrupted():
-  // nointerrpts() stops interrupts
-  // measure ADC and print to file and reset
-  // turn interrupts() back on
-  attachInterrupt(do1, on_detection, RISE);
+  attachInterrupt(io1, on_detection, RISING);
 }
-
+void loop(){
+    adc1 = analogRead(a1)
+    adc2 = analogRead(a2)
+    adc3 = analogRead(a3)
+    
+    if (adc1 > threshold){
+      digitalWrite(do1, HIGH);
+    }
+    if (adc2 > threshold){
+      digitalWrite(do2, HIGH);
+    }
+    if (adc3 > threshold){
+      digitalWrite(do3, HIGH);
+    }
+    delay(100); //wait for 0.1 second
+    digitalWrite(do1, LOW);
+    digitalWrite(do2, LOW);
+    digitalWrite(do3, LOW);
+}
+// *************************************************************************************************
 
 void on_detection(){
    noInterrupts()
@@ -53,40 +70,22 @@ void on_detection(){
    hall_probe2 = ...
    measurement_deadtime = ...
 
-   digitalWrite(do1, HIGH)  // 200 ns might be enough (test). But we can analogRead just to slow things down
-
+   digitalWrite(do1, HIGH)  // Turn on reset switch
 
    // put this between reset digital writes as delay.
-   // Serial.println(...)
-   myFile.println((String)count + " " + time_stamp+ " " + adc1 + " " adc2 + " " adc3 + get_sipm_voltage(adc1) + get_sipm_voltage(adc2)+ " " + get_sipm_voltage(adc3)+ " " + measurement_deadtime+ " " + temperatureC + ...);
+   output_str = (String)count + " " + time_stamp+ " " + adc1 + " " adc2 + " " adc3 + " " +
+                get_sipm_voltage(adc1) + get_sipm_voltage(adc2)+ " " + get_sipm_voltage(adc3)+ " " + 
+                  measurement_deadtime+ " " + temperatureC // add other variables like temperature and magnetic field later
+   Serial.println(output_str)
+   myFile.println(output_str);
    myFile.flush(); // is this needed every loop? 
-         
-   digitalWrite(do1, LOW)
-   // update measurement_deadtime
+        
+   digitalWrite(do1, LOW) // Turn off reset switch
+   
+   measurement_deadtime = ...
    interrupts()
 }
   
-void write_to_SD(){
-  while(1){
-    adc1 = analogRead(a1)
-    adc2 = analogRead(a2)
-    adc3 = analogRead(a3)
-    
-    if (adc1 > threshold){
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    if (adc2 > threshold){
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    if (adc3 > threshold){
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    delay(100); //wait for 0.1 second
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-}
-
-
 void benchmark_di(){ // do the same for analogRead.
   // start timer
   for i // 1000:
